3204923a913e66302d7c85fe194a15744a2775bf
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-05-05 19:27:35 +0200

    refactoring

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,27 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { addContact, deleteContact, fetchContacts } from './contactsOperation';
 
 const handlePending = state => {
   state.isLoading = true;
 };
-
-const handleFulfilled = (state, { payload }) => {
+const handleFulfilled = state => {
   state.isLoading = false;
   state.error = null;
+};
+const handleFulfilledFetchAll = (state, { payload }) => {
   state.items = payload;
 };
 const handleFulfilledAdd = (state, { payload }) => {
-  state.isLoading = false;
-  state.error = null;
   state.items.push(payload);
 };
 const handleFulfilledDelete = (state, { payload }) => {
-  state.isLoading = false;
-  state.error = null;
   const index = state.items.findIndex(contact => contact.id === payload.id);
   state.items.splice(index, 1);
 };
-
 const handleRejected = (state, { payload }) => {
   state.error = payload;
   state.isLoading = false;
@@ -33,17 +29,40 @@ const contactsSlice = createSlice({
 
   extraReducers: builder => {
     builder
-      .addCase(fetchContacts.pending, handlePending)
-      .addCase(deleteContact.pending, handlePending)
-      .addCase(addContact.pending, handlePending)
-      .addCase(fetchContacts.fulfilled, handleFulfilled)
+      // .addCase(fetchContacts.pending, handlePending)
+      // .addCase(deleteContact.pending, handlePending)
+      // .addCase(addContact.pending, handlePending)
+      .addCase(fetchContacts.fulfilled, handleFulfilledFetchAll)
       .addCase(addContact.fulfilled, handleFulfilledAdd)
       .addCase(deleteContact.fulfilled, handleFulfilledDelete)
-      .addCase(fetchContacts.rejected, handleRejected)
-      .addCase(deleteContact.rejected, handleRejected)
-      .addCase(addContact.rejected, handleRejected);
-    // тут в массив может добавляться много actions
-    // .addMatcher(isAnyOf([fetchContacts.pending]), handlePending);
+      // .addCase(fetchContacts.rejected, handleRejected)
+      // .addCase(deleteContact.rejected, handleRejected)
+      // .addCase(addContact.rejected, handleRejected)
+      // тут в массив может добавляться много actions
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.fulfilled,
+          deleteContact.fulfilled,
+          addContact.fulfilled
+        ),
+        handleFulfilled
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.pending,
+          deleteContact.pending,
+          addContact.pending
+        ),
+        handlePending
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.rejected,
+          deleteContact.rejected,
+          addContact.rejected
+        ),
+        handleRejected
+      );
   },
 });
 
