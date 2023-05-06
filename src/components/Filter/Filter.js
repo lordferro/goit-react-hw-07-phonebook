f141157516bf0c554cf6c734cd3f248ad5cb673b
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-05-06 14:46:23 +0200

    update

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -6,18 +6,21 @@ import { filter } from 'redux/filterSlice';
 export const Filter = () => {
   const dispatch = useDispatch();
   const filterQuery = useSelector(selectFilter);
+    const { items } = useSelector(state => state.contacts);
 
   const filterHandler = e => dispatch(filter(e.target.value));
 
-  return (
-    <Wrapper>
-      <label htmlFor="filter">Find contacts by name</label>
-      <input
-        type="text"
-        id="filter"
-        value={filterQuery}
-        onChange={filterHandler}
-      />
-    </Wrapper>
-  );
-};
+  return    <>
+     {items.length !== 0 ?        <Wrapper>
+         <label htmlFor="filter">Find contacts by name</label>
+         <input
+           type="text"
+           id="filter"
+           value={filterQuery}
+           onChange={filterHandler}
+         />
+       </Wrapper>      : 
+       <p>no contacts</p>
+     }
+   </>
+}
